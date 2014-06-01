83be588a5e94ab4d4a462f057607595001bdf5aa
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2014-06-02 00:12:47 +0200

    First step toward a nice little app to record letters from children

--- main.py
+++ main.py
@@ -3,68 +3,56 @@ kivy.require('1.7.2')
 
 import random
 
+from kivy.lang import Builder
 from kivy.app import App
+from kivy.properties import ListProperty
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.graphics import Color, Ellipse, Line
+from kivy.graphics import Color, Ellipse, Line, Rectangle
 
 
 class Painter(Widget):
 
-    color = (0., 1., 0.4)
+    #color = (0., 1., 0.4)
+
+    def __init__(self, **kwargs):
+        super(Painter, self).__init__(**kwargs)
+        self.color = (random.random(), 1., 1.)
+
     def on_touch_down(self, evt):
-        #global color
-        #color = (random.random(), 1., 1.)
+
+        if not self.collide_point(evt.x, evt.y):
+            return
+
         with self.canvas:
-            Color(*Painter.color)
+            Color(*self.color, mode = "hsv")
             d = 10
             evt.ud["trace"] = Line(point=(evt.x, evt.y), width=5)
+            evt.ud["owner"] = id(self)
 
     def on_touch_move(self, evt):
-        evt.ud["trace"].points += (evt.x, evt.y)
+        if not self.collide_point(evt.x, evt.y):
+            return
+
+        if "owner" in evt.ud and evt.ud["owner"] == id(self):
+            evt.ud["trace"].points += (evt.x, evt.y)
+
+    def draw_background(self, arg1 = None, arg2 = None):
+
+        with self.canvas:
+            Color(1, 1, 1, 0.5)
+            Rectangle(pos= self.pos, size= self.size)
 
 
-class MultiPaintApp(App):
+    def clear_letter(self):
+        self.canvas.clear()
+        self.draw_background()
 
-    def build(self):
-        global color
+class LettersApp(App):
 
-        top = Widget()
-        painter = Painter()
-        clearBtn = Button(text="Clear !")
-        whiteBtn = Button(background_color = [1,1,1,1]) #, size=(150,150), size_hint=(.3, 1))
-        redBtn = Button(background_color = [1,0,0,1]) #, size=(150,150), size_hint=(.3, 1))
-        greenBtn = Button(background_color = [0,1,0,1]) #, size=(150,150), size_hint=(.3, 1))
-
-        top.add_widget(painter)
-        
-        layout = BoxLayout(orientation='vertical', size=(100, 400), spacing = 10)
-        layout.add_widget(whiteBtn)
-        layout.add_widget(greenBtn)
-        layout.add_widget(redBtn)
-        layout.add_widget(clearBtn)
-        top.add_widget(layout)
-
-        def set_white(obj):
-            Painter.color = (1., 1., 0.7)
-
-
-        def set_green(obj):
-            Painter.color = (0., 1., 0.4)
-
-        def set_red(obj):
-            Painter.color = (1., 0., 0.)
-
-        def clear_canvas(obj):
-            painter.canvas.clear()
-
-        clearBtn.bind(on_release=clear_canvas)
-        redBtn.bind(on_release=set_red)
-        greenBtn.bind(on_release=set_green)
-        whiteBtn.bind(on_release=set_white)
-
-        return top
+    def next_word(self):
+        print("Next word!")
 
 if __name__ == "__main__":
-    MultiPaintApp().run()
+    LettersApp().run()
